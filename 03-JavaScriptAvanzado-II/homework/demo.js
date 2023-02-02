440b8d5677f3ff26002073429569e40e4911e442
luisalfonsolopez <luisalfonsolopezmora@gmail.com>
2023-02-02 10:22:05 -0500

    homework 2

---
+++ 03-JavaScriptAvanzado-II/homework/demo.js
@@ -0,0 +1,28 @@
+function saludar(saludo) {
+    return function(nombre) {
+        console.log(saludo + ' ' + nombre);
+    }
+}
+
+var saludarHola = saludar('Hola'); // Esto devuelve una función
+
+saludarHola('Toni'); // 'Hola Toni'
+
+var creaFuncion = function() {
+    var arreglo = [];
+
+    for (var i = 0; i < 3; i++) {
+        arreglo.push(
+            function() {
+                console.log(i);
+            }
+        )
+    }
+    return arreglo;
+}
+
+var arr = creaFuncion();
+
+arr[0]() // 3 sale un 3, qué esperaban ustedes??
+arr[1]() // 3
+arr[2]() // 3
